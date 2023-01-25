8ce6f436f2e63c101a4f83ed3c7e0df47a83e24a
Fabiano Muscardi <fabiano.junior@araujo.com.br>
2023-01-25 15:05:51 -0300

    Criado a entidade Empresa

---
+++ Implementação/Back-end/comprometimento-financeiro/src/main/java/com/api/compromentimentofinanceiro/models/EmpresaModel.java
@@ -0,0 +1,28 @@
+package com.api.compromentimentofinanceiro.models;
+
+import java.io.Serializable;
+import java.rmi.server.UID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "TB_EMPRESA")
+public class EmpresaModel implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UID id;
+	@Column(nullable = false, unique = true, length = 10)
+	private String nome;
+	
+}
