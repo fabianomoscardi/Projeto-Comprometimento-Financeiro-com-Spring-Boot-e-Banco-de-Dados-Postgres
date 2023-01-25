32534b2b54c9834d996d91acc3bd49651526edc2
Fabiano Muscardi <fabiano.junior@araujo.com.br>
2023-01-25 19:58:06 -0300

    Criado metódo que consulta o total do comprometimento financeiro de uma empresa na classe EmpresaRepository

--- Implementação/Back-end/comprometimento-financeiro/src/main/java/com/api/compromentimentofinanceiro/repositories/EmpresaRepository.java
+++ Implementação/Back-end/comprometimento-financeiro/src/main/java/com/api/compromentimentofinanceiro/repositories/EmpresaRepository.java
@@ -4,6 +4,7 @@ import java.rmi.server.UID;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.api.compromentimentofinanceiro.models.EmpresaModel;
@@ -12,5 +13,15 @@ import com.api.compromentimentofinanceiro.models.EmpresaModel;
 public interface EmpresaRepository extends JpaRepository<EmpresaModel, UID>{
 
 	List<EmpresaModel> findByCnpj(Integer cnpj);
+	
+	@Query(
+			value = "SELECT COUNT(p.VALOR) AS COMPROMETIMENTO_FINANCEIRO FROM TB_PROPIEDADE pe"
+					+ " INNER JOIN TB_EMPRESA ep ON pe.EMPRESAID = ep.EMPRESAID"
+					+ " INNER JOIN TB_PESSOA_FISICA pf ON ep.PESSOAFISICAID = pf.PESSOAFISICAID"
+					+ " INNER JOIN TB_PESSOA_JURIDICA pj ON ep.PESSOAJURIDICAID = pj.PESSOAJURIDICAID"
+					+ "WHERE ep.EMPRESAID = ?1"
+
+			)
+	Double consultarComprometimentoFinanceiro(UID empresaid);
 	
 }
