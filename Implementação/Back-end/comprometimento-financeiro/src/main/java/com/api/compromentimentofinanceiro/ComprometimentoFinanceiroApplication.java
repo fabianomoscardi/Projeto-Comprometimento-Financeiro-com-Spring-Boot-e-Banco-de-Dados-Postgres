b741a35b690337996cac945a57333ffa9ae49fc3
Fabiano Muscardi <fabiano.junior@araujo.com.br>
2023-01-25 13:57:08 -0300

    Criado o projeto

---
+++ Implementação/Back-end/comprometimento-financeiro/src/main/java/com/api/compromentimentofinanceiro/ComprometimentoFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.ms.compromentimentofinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsComprometimentoFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsComprometimentoFinanceiroApplication.class, args);
+	}
+
+}
---
+++ Implementação/Back-end/comprometimento-financeiro/src/test/java/com/ms/compromentimentofinanceiro/MsComprometimentoFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.compromentimentofinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsComprometimentoFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
