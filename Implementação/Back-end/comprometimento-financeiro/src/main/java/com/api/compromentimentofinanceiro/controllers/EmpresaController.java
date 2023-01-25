272d8a44c629d23ed3e644a0d771f9608cd2b297
Fabiano Muscardi <fabiano.junior@araujo.com.br>
2023-01-25 18:36:17 -0300

    Criado o Controller Empresa

---
+++ Implementação/Back-end/comprometimento-financeiro/src/main/java/com/api/compromentimentofinanceiro/controllers/EmpresaController.java
@@ -0,0 +1,62 @@
+package com.api.compromentimentofinanceiro.controllers;
+
+import java.rmi.server.UID;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.compromentimentofinanceiro.dtos.EmpresaDto;
+import com.api.compromentimentofinanceiro.models.EmpresaModel;
+import com.api.compromentimentofinanceiro.services.EmpresaService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/comprometimento-financeiro")
+public class EmpresaController {
+
+	@Autowired
+	EmpresaService empresaService;
+
+	@PostMapping("/cadastrar-empresa")
+	public ResponseEntity<EmpresaModel> cadastrarEmpresa(@RequestBody @Valid EmpresaDto empresaDto) {
+		EmpresaModel empresaModel = new EmpresaModel();
+		BeanUtils.copyProperties(empresaDto, empresaModel);
+		empresaService.cadastrarEmpresa(empresaModel);
+		return new ResponseEntity<>(empresaModel, HttpStatus.CREATED);
+	}
+
+
+	@GetMapping("/consultar-empresa/{empresaId}")
+	public Optional<EmpresaModel> consultarEmpresa(@PathVariable UID empresaId) {
+		return empresaService.consultarEmpresa(empresaId);
+	}
+	
+	@GetMapping("/consultar-comprometimento/{empresaId}")
+	public Optional<EmpresaModel> consultarComprometimentoEmpresa(@PathVariable UID empresaId) {
+		Double comprometimento = 0.0;
+		empresaService.consultarEmpresa(empresaId);
+		return null;
+	}
+	
+	
+	@DeleteMapping("/deletar-empresa/{empresaId}")
+	public ResponseEntity<UID> cadastrarEmpresa(@PathVariable UID empresaId) {
+		empresaService.deletarEmpresa(empresaId);
+		return new ResponseEntity<>(empresaId, HttpStatus.OK);
+	}
+	
+
+}
