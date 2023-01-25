ac5a8e216aa04bc56d794b6b6d594ca221247e76
Fabiano Muscardi <fabiano.junior@araujo.com.br>
2023-01-25 20:36:41 -0300

    Alterado a annotation @Data e o UUID

--- Implementação/Back-end/comprometimento-financeiro/src/main/java/com/api/compromentimentofinanceiro/models/SociedadeModel.java
+++ Implementação/Back-end/comprometimento-financeiro/src/main/java/com/api/compromentimentofinanceiro/models/SociedadeModel.java
@@ -2,22 +2,29 @@ package com.api.compromentimentofinanceiro.models;
 
 import java.util.UUID;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "TB_SOCIEDADE")
 public class SociedadeModel {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private UUID sociedadeId;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	private String sociedadeId;
 	@Column(nullable = false, unique = true, length = 10)
 	private int socio;
 }
