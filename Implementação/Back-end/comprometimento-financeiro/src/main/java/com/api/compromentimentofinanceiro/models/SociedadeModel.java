b9473daadff0627deb378abbdafb3758a177b530
Fabiano Muscardi <fabiano.junior@araujo.com.br>
2023-01-25 18:38:14 -0300

    Reformulado atributos da entidade Sociedade

--- Implementação/Back-end/comprometimento-financeiro/src/main/java/com/api/compromentimentofinanceiro/models/SociedadeModel.java
+++ Implementação/Back-end/comprometimento-financeiro/src/main/java/com/api/compromentimentofinanceiro/models/SociedadeModel.java
@@ -14,21 +14,10 @@ import lombok.Data;
 @Data
 @Table(name = "TB_SOCIEDADE")
 public class SociedadeModel {
-	
-	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private UUID id;
+	private UUID sociedadeId;
 	@Column(nullable = false, unique = true, length = 10)
 	private int socio;
-	@Column(nullable = false, unique = true, length = 10)
-	private String nome;
-	@Column(nullable = false, unique = true, length = 10)
-	private Double value;
-	
-	
-	
-	
-
 }
