c7fc71f14524d323b93550b6cdc744d2031972c3
Fabiano Muscardi <fabiano.junior@araujo.com.br>
2023-01-25 18:31:42 -0300

    Criado o service Empresa

---
+++ Implementação/Back-end/comprometimento-financeiro/src/main/java/com/api/compromentimentofinanceiro/services/EmpresaService.java
@@ -0,0 +1,45 @@
+package com.api.compromentimentofinanceiro.services;
+
+import java.rmi.server.UID;
+import java.util.Locale;
+import java.util.Optional;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.compromentimentofinanceiro.models.EmpresaModel;
+import com.api.compromentimentofinanceiro.repositories.EmpresaRepository;
+
+@Service
+public class EmpresaService {
+	
+
+	@Autowired
+	EmpresaRepository empresaRepository;
+
+	public void cadastrarEmpresa(EmpresaModel empresaModel) {
+		SociedadeService sociedadeService = new SociedadeService();
+		sociedadeService.cadastrarSociedade(empresaModel.getSociedade());
+		empresaRepository.save(empresaModel);
+	}
+
+	public Optional<EmpresaModel> consultarEmpresa(UID empresaId) {
+		return empresaRepository.findById(empresaId);
+	}
+
+	public void deletarEmpresa(UID empresaId) {
+		empresaRepository.deleteById(empresaId);
+	}
+
+	public String toString(EmpresaModel empresaModel) {
+	
+		return "O valor do comprometimento financeiro da empresa"
+				+ empresaModel.getNome() 
+				+ " é de" 
+				+ empresaModel.getComprometimento() 
+				+ ".";
+	}
+
+}
